2e138d553294c39f2a4d4d744c556fc7c5caffbd
James Sheen <jsheen@C02QF6Y4GG7J.local>
2015-11-03 17:17:55 +0000

    Changed order of stylesheets

--- header.php
+++ header.php
@@ -19,10 +19,21 @@
                 <![endif]-->
                 <!-- Kept hard coded link just in case wiredep doesn't auto include foundation -->
                 <!-- <link href="<?php // echo get_template_directory_uri();?>/bower_components/foundation/css/foundation.css" rel="stylesheet"> -->
-                 <link href="<?php echo get_template_directory_uri();?>/css/global.css" rel="stylesheet">
-                <!-- bower:css --><!-- endbower -->
+                 
+                <!-- bower:css -->
+                <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/bower_components/foundation/css/foundation.css" />
+                <!-- endbower -->
 
-                 <!-- bower:js --><!-- endbower -->
+                <link href="<?php echo get_template_directory_uri();?>/css/global.css" rel="stylesheet">
+
+                 <!-- bower:js -->
+                 <script src="<?php echo get_template_directory_uri();?>/bower_components/modernizr/modernizr.js" type="text/javascript"></script>
+                 <script src="<?php echo get_template_directory_uri();?>/bower_components/jquery/dist/jquery.js" type="text/javascript"></script>
+                 <script src="<?php echo get_template_directory_uri();?>/bower_components/fastclick/lib/fastclick.js" type="text/javascript"></script>
+                 <script src="<?php echo get_template_directory_uri();?>/bower_components/jquery.cookie/jquery.cookie.js" type="text/javascript"></script>
+                 <script src="<?php echo get_template_directory_uri();?>/bower_components/jquery-placeholder/jquery.placeholder.js" type="text/javascript"></script>
+                 <script src="<?php echo get_template_directory_uri();?>/bower_components/foundation/js/foundation.js" type="text/javascript"></script>
+                 <!-- endbower -->
             </head>
             <header>
                 <div class="wrap">
