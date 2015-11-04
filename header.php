50c90b4273be3fddb2eda3c78bb792897395f6a2
James Sheen <jsheen@C02QF6Y4GG7J.local>
2015-11-04 10:40:08 +0000

    Updated grunt remove task to include node_modules

--- header.php
+++ header.php
@@ -20,20 +20,11 @@
                 <!-- Kept hard coded link just in case wiredep doesn't auto include foundation -->
                 <!-- <link href="<?php // echo get_template_directory_uri();?>/bower_components/foundation/css/foundation.css" rel="stylesheet"> -->
                  
-                <!-- bower:css -->
-                <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/bower_components/foundation/css/foundation.css" />
-                <!-- endbower -->
+                <!-- bower:css --><!-- endbower -->
 
                 <link href="<?php echo get_template_directory_uri();?>/css/global.css" rel="stylesheet">
 
-                 <!-- bower:js -->
-                 <script src="<?php echo get_template_directory_uri();?>/bower_components/modernizr/modernizr.js" type="text/javascript"></script>
-                 <script src="<?php echo get_template_directory_uri();?>/bower_components/jquery/dist/jquery.js" type="text/javascript"></script>
-                 <script src="<?php echo get_template_directory_uri();?>/bower_components/fastclick/lib/fastclick.js" type="text/javascript"></script>
-                 <script src="<?php echo get_template_directory_uri();?>/bower_components/jquery.cookie/jquery.cookie.js" type="text/javascript"></script>
-                 <script src="<?php echo get_template_directory_uri();?>/bower_components/jquery-placeholder/jquery.placeholder.js" type="text/javascript"></script>
-                 <script src="<?php echo get_template_directory_uri();?>/bower_components/foundation/js/foundation.js" type="text/javascript"></script>
-                 <!-- endbower -->
+                 <!-- bower:js --><!-- endbower -->
             </head>
             <header>
                 <div class="wrap">
